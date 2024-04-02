6271f9336c41a130344c7cb358e2c7356e7bea1b
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-04-03 03:52:00 +0500

    My first project in TS

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <JobsList :jobs="jobs" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Job from '@/types/Job'
+import JobsList from './components/JobsList.vue'
+
+const jobs = ref<Job[]>([
+  {
+    title: 'Nurjakhon',
+    location: 'Tashkent',
+    salary: 30000,
+    id: '1'
+  },
+  {
+    title: 'Jakhon',
+    location: 'Parkent',
+    salary: 10000,
+    id: '2'
+  },
+  {
+    title: 'Khon',
+    location: 'Tomchi',
+    salary: 2000,
+    id: '3'
+  }
+])
+</script>
+
+<style scoped></style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="jobs-list">
+    <ul>
+      <li v-for="job in jobs" :key="jobs.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="ssolary">
+          <p>{{ job.salary }} $ dollar</p>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, neque!</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, PropType } from 'vue'
+import Job from '@/types/Job'
+const props = defineProps({
+  jobs: {
+    required: true,
+    type: Array as PropType<Job[]>
+  }
+})
+</script>
+
+<style scoped></style>
