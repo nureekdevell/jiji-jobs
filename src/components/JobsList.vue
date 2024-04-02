5f52418cc7ebaab15bd921083d0c4fcd6542094f
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-04-03 04:00:51 +0500

    My first project in TS css

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -6,7 +6,9 @@
         <div class="ssolary">
           <p>{{ job.salary }} $ dollar</p>
         </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, neque!</p>
+        <div class="description">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, neque!</p>
+        </div>
       </li>
     </ul>
   </div>
@@ -23,4 +25,35 @@ const props = defineProps({
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.job-list {
+  color: #17bf66;
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+</style>
